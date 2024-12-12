791bd5162f436f7eeb65aea12b9c1355ea7fa93b
jubayerkhan <jubayerkhan741@gmail.com>
2024-12-13 00:17:07 +0600

    header and footer section

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,41 +1,61 @@
 const Footer = () => {
     return (
-        <footer className="footer bg-base-200 text-base-content p-10">
-            <nav>
-                <h6 className="footer-title">Services</h6>
-                <a className="link link-hover">Branding</a>
-                <a className="link link-hover">Design</a>
-                <a className="link link-hover">Marketing</a>
-                <a className="link link-hover">Advertisement</a>
+        <footer>
+            <div  className="footer bg-[#905a00] text-white p-10">
+            <nav className="ml-32">
+                <img className="w-40" src="/src/assets/image/logo.png" alt="" />
+                <p className="w-80">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal.</p>
             </nav>
             <nav>
-                <h6 className="footer-title">Company</h6>
-                <a className="link link-hover">About us</a>
-                <a className="link link-hover">Contact</a>
-                <a className="link link-hover">Jobs</a>
-                <a className="link link-hover">Press kit</a>
+                <h6 className="footer-title">Information</h6>
+                <a className="link link-hover">Contact us</a>
+                <a className="link link-hover">Privacy Policy</a>
+                <a className="link link-hover">Terms & Condition</a>
             </nav>
             <nav>
-                <h6 className="footer-title">Legal</h6>
-                <a className="link link-hover">Terms of use</a>
-                <a className="link link-hover">Privacy policy</a>
-                <a className="link link-hover">Cookie policy</a>
+                <h6 className="footer-title">Help & Support</h6>
+                <div className="flex gap-2">
+                    <img className="w-5 h-4 mt-1" src="/src/assets/image/email.png" alt="" />
+                    <p>Email: </p>
+                    <div>
+                        <p>Youremail@gmail.com</p>
+                        <p>Letstalk@gmail.com</p>
+                    </div>
+                </div>
+                <div className="flex gap-2">
+                    <img className="w-5 h-4 mt-1" src="/src/assets/image/phone-call.png" alt="" />
+                    <p>Phone: </p>
+                    <div>
+                        <p>(+)(888)750-6866</p>
+                        <p>(+)(888)785-3986</p>
+                    </div>
+                </div>
             </nav>
             <form>
-                <h6 className="footer-title">Newsletter</h6>
+                <h6 className="footer-title">Newsletter Sign Up</h6>
                 <fieldset className="form-control w-80">
                     <label className="label">
-                        <span className="label-text">Enter your email address</span>
+                        <span className="label-text text-white">Get Alert Directly Into Your Inbox <br />
+                        After Each Post.</span>
                     </label>
-                    <div className="join">
+                    <div className="join rounded-3xl">
                         <input
                             type="text"
-                            placeholder="username@site.com"
+                            placeholder="Enter Your Mail"
                             className="input input-bordered join-item" />
-                        <button className="btn btn-primary join-item">Subscribe</button>
+                        <button className="btn join-item"><img className="w-8" src="/src/assets/image/telegram.png" alt="" /></button>
                     </div>
                 </fieldset>
             </form>
+            </div>
+            <div className="flex justify-between text-white bg-black">
+                <p className="ml-20">©2024Spaktech. Agency | All Right Reserved</p>
+                <div className="flex gap-3 mr-20 ">
+                    <img className="w-5 h-5 bg-white" src="/src/assets/image/facebook.png" alt="" />
+                    <img className="w-5 h-5 bg-white" src="/src/assets/image/instagram.png" alt="" />
+                    <img className="w-5 h-5" src="/src/assets/image/twitter.png" alt="" />
+                </div>
+            </div>
         </footer>
     );
 };
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,21 @@
 const Home = () => {
     return (
         <div>
-            <h2>This is home</h2>
+            {/* banner sectoin */}
+            <div
+                className="hero min-h-screen"
+                style={{
+                    backgroundImage: "url(https://images.pexels.com/photos/7031705/pexels-photo-7031705.jpeg)",
+                }}>
+                <div className="hero-overlay bg-opacity-60"></div>
+                <div className="hero-content text-neutral-content text-center">
+                    <div className="max-w-md">
+                        <h1 className="mb-5 text-4xl">Functional Movement. <br />
+                        Anytime Anywhere.</h1>
+                        <button className="btn bg-[#B47000] text-white">Join our Class <img src="/src/assets/image/diagonal-arrow.png" alt="" /> </button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
